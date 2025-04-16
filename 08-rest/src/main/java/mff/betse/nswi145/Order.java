c6e3c0d67e0f3d686f3886ea54c11f447630e7de
betse <betse11001@gmail.com>
2025-04-17 01:45:59 +0200

    fix xml

--- 08-rest/src/main/java/mff/betse/nswi145/Order.java
+++ 08-rest/src/main/java/mff/betse/nswi145/Order.java
@@ -3,10 +3,14 @@ package mff.betse.nswi145;
 import java.util.List;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
 import jakarta.xml.bind.annotation.XmlElement;
 import jakarta.xml.bind.annotation.XmlRootElement;
 
-@XmlRootElement
+@XmlRootElement(name="order")
+@XmlAccessorType(XmlAccessType.FIELD)
+@Schema(description = "An order of food items")
 public class Order {
     @Schema(description = "Unique order ID", example = "ORD12345")
     private String id;
--- 08-rest/src/main/java/mff/betse/nswi145/RestConfig.java
+++ 08-rest/src/main/java/mff/betse/nswi145/RestConfig.java
@@ -3,12 +3,19 @@ package mff.betse.nswi145;
 import io.swagger.v3.jaxrs2.integration.resources.OpenApiResource;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import jakarta.ws.rs.ApplicationPath;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.jackson.JacksonFeature;
 
 @ApplicationPath("/api")
 @OpenAPIDefinition(
+		servers = {
+			    @Server(
+			      description = "Default server",
+			      url         = "/08-rest"
+			    )
+			  },
   info = @Info(
     title       = "Order Service API",
     version     = "v1",
